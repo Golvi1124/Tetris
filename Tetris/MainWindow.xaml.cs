1ed60c6e7dde290b635bff74ab40ed831bf37238
Golvi1124 <golvi1124@gmail.com>
2025-06-06 10:37:20 +0200

    Setting Up the Canvas and Drawing the Game Grid

--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -9,16 +9,108 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Tetris
+namespace Tetris;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private readonly ImageSource[] tileImages = new ImageSource[]
     {
-        public MainWindow()
+        new BitmapImage(new Uri("Assets/TileEmpty.png", UriKind.Relative)),
+        new BitmapImage(new Uri("Assets/TileCyan.png", UriKind.Relative)),
+        new BitmapImage(new Uri("Assets/TileBlue.png", UriKind.Relative)),
+        new BitmapImage(new Uri("Assets/TileOrange.png", UriKind.Relative)),
+        new BitmapImage(new Uri("Assets/TileYellow.png", UriKind.Relative)),
+        new BitmapImage(new Uri("Assets/TileGreen.png", UriKind.Relative)),
+        new BitmapImage(new Uri("Assets/TilePurple.png", UriKind.Relative)),
+        new BitmapImage(new Uri("Assets/TileRed.png", UriKind.Relative)),
+    };
+
+    private readonly ImageSource[] blockImages = new ImageSource[]
+    {
+        new BitmapImage(new Uri("Assets/Block-Empty.png", UriKind.Relative)), 
+        new BitmapImage(new Uri("Assets/BlockI.png", UriKind.Relative)),
+        new BitmapImage(new Uri("Assets/BlockJ.png", UriKind.Relative)),
+        new BitmapImage(new Uri("Assets/BlockL.png", UriKind.Relative)),
+        new BitmapImage(new Uri("Assets/BlockO.png", UriKind.Relative)),
+        new BitmapImage(new Uri("Assets/BlockS.png", UriKind.Relative)),
+        new BitmapImage(new Uri("Assets/BlockT.png", UriKind.Relative)),
+        new BitmapImage(new Uri("Assets/BlockZ.png", UriKind.Relative))
+    };
+
+    private readonly Image[,] imageControls;
+
+    private GameState gameState = new GameState();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        imageControls = SetupGameCanvas(gameState.GameGrid);
+    }
+
+    private Image[,] SetupGameCanvas(GameGrid grid)
+    {
+        Image[,] imageControls = new Image[grid.Rows, grid.Columns];
+        int cellSize = 25; // size of each cell in pixels
+
+        for (int r = 0; r < grid.Rows; r++)
         {
-            InitializeComponent();
+            for (int c = 0; c < grid.Columns; c++)
+            {
+                Image imageControl = new Image
+                {
+                    Width = cellSize,
+                    Height = cellSize
+                };
+
+                Canvas.SetTop(imageControl, (r - 2) * cellSize); // push 2 invisible cells up, so they are not visible
+                Canvas.SetLeft(imageControl, c * cellSize);
+                GameCanvas.Children.Add(imageControl);
+                imageControls[r, c] = imageControl; // store the image control in the array
+            }
         }
+        return imageControls;
+    }
+
+    private void DrawGrid(GameGrid grid)
+    {
+        for (int r = 0; r < grid.Rows; r++)
+        {
+            for (int c = 0; c < grid.Columns; c++)
+            {
+                int id = grid[r, c];
+                imageControls[r, c].Source = tileImages[id];
+            }
+        }
+    }
+
+    private void DrawBlock(Block block)  //draw the current block on the game canvas
+    {
+        foreach (Position p in block.TilePositions())
+        {
+            imageControls[p.Row, p.Column].Source = tileImages[block.Id];
+        }
+    }
+
+    private void Draw(GameState gameState)
+    {
+        DrawGrid(gameState.GameGrid);
+        DrawBlock(gameState.CurrentBlock);
+    }
+
+    private void Window_KeyDown(object sender, KeyEventArgs e)
+    {
+
+    }
+    private void GameCanvas_Loaded(object sender, RoutedEventArgs e)
+    {
+        Draw(gameState);
+    }
+
+    private void PlayAgain_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
